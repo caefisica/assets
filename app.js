53c4d166a28f1bc72bad3ffcb09681906b1035b5
totallynotdavid <dadch1404@gmail.com>
2022-02-10 01:56:58 -0500

    Fix name again

--- app.js
+++ app.js
@@ -6263,7 +6263,7 @@ function file_video(path) {
     )
     .join("");
   player_items += `<li class="mdui-divider"></li>
-                   <li class="mdui-menu-item"><a id="copy-link" class="mdui-ripple">Copy Link</a></li>`;
+                   <li class="mdui-menu-item"><a id="copy-link" class="mdui-ripple">Copiar enlace</a></li>`;
   const playBtn = `
       <button class="mdui-btn mdui-ripple mdui-color-theme-accent" mdui-menu="{target:'#player-items'}">
         <i class="mdui-icon material-icons">&#xe039;</i>Play In External Player<i class="mdui-icon material-icons">&#xe5cf;</i>
